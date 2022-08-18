3d117ec04d16deb837c7845053be09d2f529d8af
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-08-18 17:35:52 -0500

    committing work before wifi goes out haha

--- client/src/components/PostList.js/index.js
+++ client/src/components/PostList.js/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+const { id: postId } = useParams();
 
 const PostList = ({ posts, title }) => {
   if (!posts.length) {
