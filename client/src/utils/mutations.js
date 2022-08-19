3c4ee9a6d0e31180b45f1c6fb51cc6ffdd31f7af
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-08-19 13:50:18 -0500

    updating mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,12 +12,71 @@ export const LOGIN_USER = gql`
   }
 `;
 
+export const ADD_POST = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
+        username
+      }
+    }
+  }
+`;
+
+
+export const ADD_EMPATH = gql`
+  mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+      _id
+      username
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_EMPATH = gql`
+  mutation removeFriend($id: ID!) {
+    removeFriend(id: $id) {
+      _id
+      username
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_REACTION = gql`
+  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
         username
       }
     }
