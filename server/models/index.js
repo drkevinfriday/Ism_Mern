b8e46621078f8b77e8f0e6a45d516754aa225329
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-08-25 14:45:39 -0500

    test with Victor

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const User = require('./User');
 const Post = require('./Post');
-const Category = require('./Category');
+const Category = require('./category');
 
 module.exports = { User, Post, Category };
